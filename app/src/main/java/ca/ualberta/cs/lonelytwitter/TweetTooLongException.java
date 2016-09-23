4c8669b2fe7ab0e154fbcd54244e324b52f2cfb3
morlick <orlick@ualberta.ca>
2016-09-22 18:15:56 -0600

    lab 2 demo

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetTooLongException.java
@@ -0,0 +1,10 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by orlick on 9/15/16.
+ */
+public class TweetTooLongException extends Exception{
+
+
+
+}
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
@@ -0,0 +1,13 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+/**
+ * Created by orlick on 9/15/16.
+ */
+public interface Tweetable {
+    public String getMessage();
+    public Date getDate();
+
+
+}
